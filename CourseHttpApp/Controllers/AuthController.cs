33e0d27a2d44807108f6d741e96f5229199a00c1
Egor <korepin.02@gmail.com>
2022-09-18 23:35:01 +0300

    Test auth

---
+++ CourseHttpApp/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using CourseHttpApp.Models;
+using CourseHttpApp.Models.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseHttpApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public HttpResponseMessage Post(string login, string password)
+    {
+        var hash = Crypt.GetHashPassword(password);
+        using var db = new ApplicationContext(_configuration.GetConnectionString("SyncDb"));
+        var result = db.Users.FirstOrDefault(item => item.login == login && item.password == hash);
+        if (result == null)
+        {
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+
+        return new HttpResponseMessage(HttpStatusCode.OK);
+    }
+}
---
+++ CourseHttpApp/Models/Common/Crypt.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CourseHttpApp.Models.Common;
+
+internal class Crypt
+{
+    internal static string GetHashPassword(string password)
+    {
+        using var crypt = SHA256.Create();
+        var hash = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+        var hashString = new StringBuilder();
+        foreach (var x in hash)
+        {
+            hashString.Append(x.ToString("X2"));
+        }
+
+        return hashString.ToString();
+    }
+}
