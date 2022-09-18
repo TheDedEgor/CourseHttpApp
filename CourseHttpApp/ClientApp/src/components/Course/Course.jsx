c14a2df1662239a71645d1d8ca88682a76ef8839
Ivan <petros_9002@mail.ru>
2022-09-18 23:37:25 +0300

    Test Auth

--- CourseHttpApp/ClientApp/src/components/Course/Course.jsx
+++ CourseHttpApp/ClientApp/src/components/Course/Course.jsx
@@ -1,9 +1,26 @@
 ﻿import React from "react";
 import "./Course.css"
-const Course = () =>{
+import {Link, Route, Routes, useParams} from 'react-router-dom'
+const Course = ({courses}) =>{
+    const params = useParams()
+    console.log(params)
     return(
-        <div>
-            Course
+        <div className="course-block">
+                {courses.map((course) =>(
+                    <div className="course">
+                        <div className="course-title">
+                            <h2>{course.name}</h2>
+                        </div>
+                        {course.dependises.map((depend) =>(
+                            <a to="">{depend.name}</a>
+                        ))}
+                    </div>
+                ))}
+            <div className="main-block">
+                <Routes>
+                    <Route path="/:id" element={<h1>sdf</h1>}/>
+                </Routes>
+            </div>
         </div>
     )
 }
---
+++ CourseHttpApp/ClientApp/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import "./Footer.css"
+const Footer = () =>{
+    return(
+        <div className="footer">
+            Footer
+        </div>
+    )
+}
+
+export default Footer;
