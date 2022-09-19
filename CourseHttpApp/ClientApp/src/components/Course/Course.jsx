c3e0a878f82025b1956f89cc254c967f7ef732cb
Ivan <petros_9002@mail.ru>
2022-09-20 00:08:50 +0300

    Developing Course component

--- CourseHttpApp/ClientApp/src/components/Course/Course.jsx
+++ CourseHttpApp/ClientApp/src/components/Course/Course.jsx
@@ -1,25 +1,44 @@
-﻿import React from "react";
+﻿import React, {useEffect, useState} from "react";
 import "./Course.css"
 import {Link, Route, Routes, useParams} from 'react-router-dom'
+import {Log} from "oidc-client";
 const Course = ({courses}) =>{
-    const params = useParams()
-    console.log(params)
+    const {id} = useParams()
+    let post = courses[0].dependises[id -1]
+    /*console.log(courses[0].dependises.dep.map(p => p.id))*/
     return(
         <div className="course-block">
                 {courses.map((course) =>(
-                    <div className="course">
+                    <div className="course" key={course.id}>
                         <div className="course-title">
                             <h2>{course.name}</h2>
                         </div>
-                        {course.dependises.map((depend) =>(
-                            <a to="">{depend.name}</a>
-                        ))}
+                        <div className="course-links">
+                            {course.dependises.map((depend) =>(
+                                <div>
+                                    <Link key={depend.id} to={`/${depend.id}`}>{depend.name}</Link>
+                                    <div>
+                                        {depend.dep.map(p => (
+                                            <div className="depend-links">
+                                                <p>{p.name}</p>
+                                                {/*<p>{p.descrip}</p>*/}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 ))}
             <div className="main-block">
-                <Routes>
-                    <Route path="/:id" element={<h1>sdf</h1>}/>
-                </Routes>
+                {/*<Routes>
+                    <Route path="/:id" element={<h1>{id}</h1>}/>
+                </Routes>*/}
+                {post && <div>
+                    <h1>{post.name}</h1>
+                    <span>{post.desc}</span>
+                    {post.image === null ? <></> :<img src={post.image} alt="post-img"/>}
+                </div>}
             </div>
         </div>
     )
