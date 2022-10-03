204c8b3b1df01aad90aca3e979b6b9dcf0f65968
Ivan <petros_9002@mail.ru>
2022-10-03 19:49:12 +0300

    Test Commit

--- CourseHttpApp/ClientApp/src/components/ForgotPass/ForgotPass.jsx
+++ CourseHttpApp/ClientApp/src/components/ForgotPass/ForgotPass.jsx
@@ -2,10 +2,26 @@
 import "./ForgotPass.css"
 
 const ForgotPass = () =>{
+    async function handleFormSubmit(event) {
+        event.preventDefault();
+        const data = serializeForm(event.target);
+        const response = await sendData(data);
+        const user = await response.json()
+    }
+    function serializeForm(formNode) {
+        return new FormData(formNode)
+    }
+
+    async function sendData(data) {
+        return await fetch('/api/Pass/', {
+            method: 'POST',
+            body: data,
+        })
+    }
     return(
         <>
             <h3>Восстановление пароля</h3>
-            <form className="form_forgot">
+            <form onSubmit={handleFormSubmit} className="form_forgot" >
                 <input name="login" placeholder="Введите email"/>
                 <input type="submit" value="Отправить"/>
             </form> 
